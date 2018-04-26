9bb70afea5a0f7c50eaec06789e7ba8fc52b752b
John Williams <john@redstubble.com>
2018-04-26 12:07:42 +1200

    TouchableHighlight Btn

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import React from 'react'
-import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { 
+  Text,
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+ } from 'react-native'
+
 
 export default class App extends React.Component {
+  handlePress = () => {
+    alert('Hello')
+  }
   render() {
     return (
-      <View>
-        <AddEntry />
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.btn} onPress={this.handlePress} underlayColor='#D4271B'>
+        <Text style={styles.btnText}>TouchableHighlight</Text>
+        </TouchableHighlight>
+        {/* <AddEntry /> */}
       </View>
     );
   }
 }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginLeft: 10,
+      marginRight: 10,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  ,
+  btn: {
+    backgroundColor: '#E53224',
+    padding:10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff'
+  }
+  })
+
+
+
