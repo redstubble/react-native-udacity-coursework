745ee0feb7fd56ad067f9d0d882804ad52bb2ceb
John Williams <john@redstubble.com>
2018-04-27 12:17:33 +1200

    Displaying Lists - List View Non-Scrollable

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
 import React from 'react'
-import AddEntry from './components/AddEntry'
-import { View } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import getReviews from './reviews'
 
+function Review({ name, text, avatar }) {
+  return (
+    <View style={styles.review}>
+      <Image source={{uri: avatar}} style={styles.avatar}/>
+      <View style={{flex: 1, flexWrap: 'wrap'}}>
+        <Text style={{fontSize: 20}}>{name}</Text>
+        <Text>{text}</Text>
+      </View>
+    </View>
+  )
+}
 
 export default class App extends React.Component {
-
   render() {
+    const reviews = getReviews()
     return (
-      <View>
-        <AddEntry/>
+      <View style={styles.container}>
+        {reviews.map(({name, text, avatar}, key) => 
+          <Review key={key} name={name} text={text} avatar={avatar} />
+        )}
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  review: {
+    margin:10,
+    flexDirection: 'row',
+  },
+  avatar: {
+    height:50,
+    width: 50,
+    borderRadius:25
+  }
+})
 
 
---
+++ reviews.js
@@ -0,0 +1,78 @@
+const reviews = [
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    },
+    {
+        "url": "https://twitter.com/WaNgeL_SaTar",
+        "text": "TylerMcGinnis.com - The best courses I've ever seen",
+        "name": "Taras Protchenko",
+        "key": "Taras Protchenko",
+        "avatar": "https://twitter.com/WaNgel_SaTaR/profile_image?size=normal",
+        "best":true
+    }
+]
+
+export default function getReviews() {
+    return reviews
+}
