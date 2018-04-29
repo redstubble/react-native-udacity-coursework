0b9676b975129ce18af28703e2c60f39e32955b2
John Williams <john@redstubble.com>
2018-04-30 10:55:49 +1200

    Add basic stylesheet to app

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { white } from './colors'
+import { white, red, orange, blue, lightPurp, pink } from './colors'
   
   export function isBetween (num, x, y) {
     if (num >= x && num <= y) {
@@ -45,6 +45,18 @@ import { white } from './colors'
     return todayUTC.toISOString().split('T')[0]
   }
 
+  const styles = StyleSheet.create({
+    iconContainer: {
+      padding: 5,
+      borderRadius: 8,
+      width:50,
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 20
+    }
+  })
+
   export function getMetricMetaInfo(metric) {
       const info = {
           run:{
@@ -55,7 +67,7 @@ import { white } from './colors'
               type: 'steppers',
               getIcon() {
                   return (
-                  <View>
+                  <View style={[styles.iconContainer, {backgroundColor: red}]}>
                       <MaterialIcons
                         name='directions-run'
                         color={'black'}
@@ -73,7 +85,7 @@ import { white } from './colors'
             type: 'steppers',
             getIcon() {
                 return (
-                <View>
+                <View style={[styles.iconContainer, {backgroundColor: orange}]}>
                     <MaterialCommunityIcons
                       name='bike'
                       color={'black'}
@@ -91,7 +103,7 @@ import { white } from './colors'
             type: 'steppers',
             getIcon() {
                 return (
-                <View>
+                <View style={[styles.iconContainer, {backgroundColor: blue}]}>
                     <MaterialCommunityIcons
                       name='swim'
                       color={'black'}
@@ -109,7 +121,7 @@ import { white } from './colors'
             type: 'slider',
             getIcon() {
                 return (
-                <View>
+                <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
                     <FontAwesome
                       name='bed'
                       color={'black'}
@@ -127,7 +139,7 @@ import { white } from './colors'
             type: 'slider',
             getIcon() {
                 return (
-                <View>
+                <View style={[styles.iconContainer, {backgroundColor: pink}]}>
                     <MaterialCommunityIcons
                       name='food'
                       color={'black'}
