3175520a6d119fd94086b33bad1b1291b3430652
John Williams <john@redstubble.com>
2018-05-10 14:25:26 +1200

    Add EntryDetail to redux populate view with metric card add date header

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
 
 class EntryDetail extends Component {
+    static navigationOptions = ({ navigation }) => {
+        const { entryId } = navigation.state.params
+
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+
+        return {
+            title: `${day}/${month}/${year}`
+        }
+    }
     render() {
+        const { metrics } = this.props
+
         return (
-            <View>
+            <View style={styles.container}>
+                <MetricCard metrics={metrics} />
                 <Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
             </View>
         );
     }
 }
 
-export default EntryDetail
+function mapStateToProps(state, { navigation }) {
+    const { entryId } = navigation.state.params;
+    return {
+        entryId,
+        metrics: state[entryId]
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15
+    }
+})
+
+export default connect(mapStateToProps)(EntryDetail)
